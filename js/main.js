7fd3e92b222b9e837b71fe5d2f02584dca8cc58a
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2024-05-02 18:37:57 +0300

    fix scroll

--- js/main.js
+++ js/main.js
@@ -1,28 +1,34 @@
-const orderServiceBtn = document.querySelector('.hero-image-button');
-const closeModalBtn = document.querySelector('.modal-btn');
-const modalWindow = document.querySelector('.backdrop');
+const orderServiceBtn = document.querySelector(".hero-image-button");
+const closeModalBtn = document.querySelector(".modal-btn");
+const modalWindow = document.querySelector(".backdrop");
+const modalWindowContent = document.querySelector(".modal");
 
-const burgerMenu = document.querySelector('.heder-menu-btn');
-const closeMobileMenu = document.querySelector('.menu-close-btn');
-const mobileMenu = document.querySelector('.header-mob-menu');
+const burgerMenu = document.querySelector(".heder-menu-btn");
+const closeMobileMenu = document.querySelector(".menu-close-btn");
+// const mobileMenu = document.querySelector(".header-mob-menu");
 
-orderServiceBtn.addEventListener('click', openCloseModalWindow);
-burgerMenu.addEventListener('click', openCloseMobileMenu);
+orderServiceBtn.addEventListener("click", openCloseModalWindow);
+burgerMenu.addEventListener("click", openCloseMobileMenu);
 
 function openCloseModalWindow() {
-    modalWindow.classList.toggle('is-open');
-    if(modalWindow.classList.contains('is-open')) {
-      closeModalBtn.addEventListener('click', openCloseModalWindow);
-    } else {
-      closeModalBtn.removeEventListener('click', openCloseModalWindow);
-    }
+  modalWindow.classList.toggle("is-open");
+  if (modalWindow.classList.contains("is-open")) {
+    closeModalBtn.addEventListener("click", openCloseModalWindow);
+    document.body.style.overflow = "hidden";
+    // modalWindow.style.overflow = "auto";
+  } else {
+    closeModalBtn.removeEventListener("click", openCloseModalWindow);
+    document.body.style.overflow = "auto";
+  }
 }
 
 function openCloseMobileMenu() {
-    mobileMenu.classList.toggle('is-open');
-    if(mobileMenu.classList.contains('is-open')) {
-      closeMobileMenu.addEventListener('click', openCloseMobileMenu);
-    } else {
-      closeMobileMenu.removeEventListener('click', openCloseMobileMenu);
-    }
+  mobileMenu.classList.toggle("is-open");
+  if (mobileMenu.classList.contains("is-open")) {
+    closeMobileMenu.addEventListener("click", openCloseMobileMenu);
+    document.body.style.overflow = "hidden";
+  } else {
+    closeMobileMenu.removeEventListener("click", openCloseMobileMenu);
+    document.body.style.overflow = "auto";
+  }
 }
