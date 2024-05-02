8a20caacbec6a8f779ff8f9c896ed87668c0ef1c
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2024-05-02 20:52:58 +0300

    20240502

--- js/main.js
+++ js/main.js
@@ -1,34 +1,58 @@
-const orderServiceBtn = document.querySelector(".hero-image-button");
+const openModalBtn = document.querySelector(".hero-image-button");
 const closeModalBtn = document.querySelector(".modal-btn");
-const modalWindow = document.querySelector(".backdrop");
+const backdrop = document.querySelector(".backdrop");
 
-const burgerMenuBtn = document.querySelector(".heder-menu-btn");
-const closeMobileBtn = document.querySelector(".menu-close-btn");
+const openMenuBtn = document.querySelector(".heder-menu-btn");
+const closeMenuBtn = document.querySelector(".menu-close-btn");
 const mobileMenu = document.querySelector(".header-mob-menu");
 
-orderServiceBtn.addEventListener("click", openModalWindow);
-burgerMenuBtn.addEventListener("click", openMobileMenu);
+openModalBtn.addEventListener("click", onOpenModalWindow);
+openMenuBtn.addEventListener("click", onOpenMobileMenu);
 
-function openModalWindow() {
-  modalWindow.classList.add("is-open");
-  closeModalBtn.addEventListener("click", closeModalWindow);
+function onOpenModalWindow() {
+  backdrop.classList.add("is-open");
   document.body.style.overflow = "hidden";
+  backdrop.style.overflow = "auto";
+  backdrop.addEventListener("click", onBackdrop);
+  window.addEventListener("keydown", onEsc);
+  closeModalBtn.addEventListener("click", onCloseModalWindow);
 }
 
-function closeModalWindow() {
-  modalWindow.classList.remove("is-open");
-  closeModalBtn.removeEventListener("click", closeModalWindow);
+function onCloseModalWindow() {
+  backdrop.classList.remove("is-open");
   document.body.style.overflow = "auto";
+  backdrop.style.overflow = "hidden";
+  backdrop.removeEventListener("click", onBackdrop);
+  window.removeEventListener("keydown", onEsc);
+  closeModalBtn.removeEventListener("click", onCloseModalWindow);
 }
 
-function openMobileMenu() {
+function onEsc(event) {
+  if (event.key === "Escape") {
+    onCloseModalWindow();
+  }
+}
+
+function onBackdrop(event) {
+  if (event.target === backdrop) {
+    onCloseModalWindow();
+  }
+}
+
+function onOpenMobileMenu() {
   mobileMenu.classList.add("is-open");
-  closeMobileBtn.addEventListener("click", closeMobileMenu);
   document.body.style.overflow = "hidden";
+  closeMenuBtn.addEventListener("click", onCloseMobileMenu);
 }
 
-function closeMobileMenu() {
+function onCloseMobileMenu() {
   mobileMenu.classList.remove("is-open");
-  closeMobileBtn.removeEventListener("click", closeMobileMenu);
   document.body.style.overflow = "auto";
+  closeMenuBtn.removeEventListener("click", onCloseMobileMenu);
 }
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 768 && mobileMenu.classList.contains("is-open")) {
+    onCloseMobileMenu();
+  }
+});
